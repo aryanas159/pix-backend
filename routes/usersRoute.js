2ff487562f1bac1da87e9f6d3c513f00548bc9c1
Aryan Singh <114330931+aryanas159@users.noreply.github.com>
2023-07-03 19:37:06 +0530

    making DB main

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -4,13 +4,13 @@ const {
 	getUser,
 	getUserFriends,
 	addRemoveFriend,
-    getOwnFriends
+	getUserImage
 } = require("../controllers/usersController");
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/authMiddleware");
 router.route("/").get(authMiddleware, getAllUsers);
-router.route("/friends").get(authMiddleware, getOwnFriends);
+router.route("/image/:userId").get(authMiddleware, getUserImage);
 router.route("/search").get(authMiddleware, searchUser);
 router.route("/:id").get(authMiddleware, getUser);
 router.route("/:id/friends").get(authMiddleware, getUserFriends);
