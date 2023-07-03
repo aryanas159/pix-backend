26eb89bf49ba54681182bda87ad00e694f5c3933
Aryan Singh <114330931+aryanas159@users.noreply.github.com>
2023-07-03 19:59:54 +0530

    changed cors options

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const path = require("path");
 app.use(
 	cors({
-		origin: "http://localhost:5173",
+		origin: "https://ssaryans-pix.netlify.app",
 	})
 );
 const multer = require("multer");
